5a00dbea53632939647060ae262acbb11055f1fd
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-15 14:25:00 +0530

    Update Input_sum_of_2_number.c

--- Input_sum_of_2_number.c
+++ Input_sum_of_2_number.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main() {
+    int a ,b;
+    
+    printf("enter a \n");
+    scanf("%d", &a);
+    
+    printf("enter b \n");
+    scanf("%d", &b);
+    printf("sum of two number is : %d \n", a + b);
+    
+    return 0;
+}
