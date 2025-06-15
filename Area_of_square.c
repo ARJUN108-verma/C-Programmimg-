fef7b211a11886394818ae2c1a28d5b7e7652bd4
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-15 14:28:40 +0530

    Update Area_of_square.c

--- Area_of_square.c
+++ Area_of_square.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main() {
+    int side;
+    
+    printf("enter side \n");
+    scanf("%d", &side);
+    
+    printf("Area of square is : %d \n", side * side);
+    
+    return 0;
+}
