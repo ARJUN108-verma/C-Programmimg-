7a3ef519f09271c8bd6d12737d01e76e967aebd2
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-16 17:16:34 +0530

    Update Type_declaration.c

--- Type_declaration.c
+++ Type_declaration.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+int main() {
+    int age = 27;
+    int old_age = age;
+    int new_age = old_age + 2;
+    
+    printf("new age is : %d", new_age);
+    
+    int rupee = 1, dollar = 87;
+    
+     /* //valid declaration 
+    int age1, age2, age3 = 27 */
+    
+}
