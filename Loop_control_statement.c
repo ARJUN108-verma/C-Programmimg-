791f9f0fea0a7f3135e6a792b0e6c812ac420e3f
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-20 11:14:29 +0530

    Update Loop_control_statement.c

--- Loop_control_statement.c
+++ Loop_control_statement.c
@@ -1,1 +1,30 @@
+// Loop Control Statements
+#include<stdio.h>
 
+int main(){
+    // for loop
+    int i;
+    for(i = 1; i < 11; i++){
+        printf("%d \n" ,i);
+    }
+    
+    // while loop
+    int j = 1;
+    while(j <= 10){
+        printf("%d \n", j );
+        j++;
+    }
+    
+    // do while loop
+    int k =1;
+    do{
+        printf("%d \n", k);
+        k++;
+    }
+    while(k <= 15);
+    return 0;
+    
+
+    
+    
+}
