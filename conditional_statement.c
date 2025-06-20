bd4716913d56aa993d25d1f84c7133b0c3f26da1
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-20 10:51:31 +0530

    Update conditional_statement.c

--- conditional_statement.c
+++ conditional_statement.c
@@ -1,1 +1,24 @@
+// Condition statement 
+#include<stdio.h>
 
+int main(){
+    // check for voting 
+    int age = 21;
+    if (age >= 18){
+        printf("you are eligible for voting \n");
+    }
+    else {
+        printf("you are eligible for voting \n");
+    }
+    
+    // checking a number is odd or even
+    
+    int num = 3;
+    if (num % 2 == 0){
+        printf("even number \n");
+    }
+    else;{
+        printf("odd numner \n");
+    }
+    
+}
