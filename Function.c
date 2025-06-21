1b674e412f9a899ec368c7f36d32ab84ae7607c4
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-21 09:49:24 +0530

    Update Function.c

--- Function.c
+++ Function.c
@@ -1,1 +1,18 @@
+// Function
+#include<stdio.h>
 
+//function declaration
+void printHello();
+int calcSquare(int n);
+
+int main() {
+    //function call
+    printHello();
+    return 0;
+    
+}
+
+// function definition
+void  printHello() {
+    printf("Hello! \n");
+}
